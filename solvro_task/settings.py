4ab4672a121f794e60d45ca7e89077cdcc9f9621
ixgvah <igaposturzynska@gmail.com>
2026-03-22 23:03:54 +0100

    Changed settings

--- solvro_task/settings.py
+++ solvro_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-te^srwr8kbm41@!rf-#2guiy=!%c#wpyx*s)ms@-n=vkg4wdpm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ixgvah.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
